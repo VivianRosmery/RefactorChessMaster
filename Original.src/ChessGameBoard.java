90321556d31653db19f962fe0025d432bc28b5ea
Vivian Lazaro <vivian.lazaro@unmsm.edu.pe>
2023-02-27 13:51:35 -0500

    2 parte de la octava refactorización

--- Original.src/ChessGameBoard.java
+++ Original.src/ChessGameBoard.java
@@ -196,18 +196,14 @@ public class ChessGameBoard extends JPanel{
         for ( int i = 0; i < chessCells.length; i++ ){
             for ( int j = 0; j < chessCells[0].length; j++ ){
                 ChessGamePiece pieceToAdd;
-                if ( i == 1 ) // black pawns
-                {
+                if ( i == 1 ){ // black pawns
                     pieceToAdd = new Pawn( this, i, j, ChessGamePiece.BLACK );
                 }
-                else if ( i == 6 ) // white pawns
-                {
+                else if ( i == 6 ){ // white pawns
                     pieceToAdd = new Pawn( this, i, j, ChessGamePiece.WHITE );
                 }
-                else if ( i == 0 || i == 7 ) // main rows
-                {
-                    int colNum =
-                        i == 0 ? ChessGamePiece.BLACK : ChessGamePiece.WHITE;
+                else if ( i == 0 || i == 7 ){ // main rows
+                    int colNum = i == 0 ? ChessGamePiece.BLACK : ChessGamePiece.WHITE;
                     if ( j == 0 || j == 7 ){
                         pieceToAdd = new Rook( this, i, j, colNum );
                     }
@@ -220,21 +216,18 @@ public class ChessGameBoard extends JPanel{
                     else if ( j == 3 ){
                         pieceToAdd = new King( this, i, j, colNum );
                     }
-                    else
-                    {
+                    else {
                         pieceToAdd = new Queen( this, i, j, colNum );
                     }
                 }
-                else
-                {
+                else {
                     pieceToAdd = null;
                 }
                 chessCells[i][j] = new BoardSquare( i, j, pieceToAdd );
                 if ( ( i + j ) % 2 == 0 ){
                     chessCells[i][j].setBackground( Color.WHITE );
                 }
-                else
-                {
+                else {
                     chessCells[i][j].setBackground( Color.BLACK );
                 }
                 chessCells[i][j].addMouseListener( listener );
